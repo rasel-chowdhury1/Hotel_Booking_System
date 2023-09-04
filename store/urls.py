b857f83f03579a5729161738f3a138f2f0917f87
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2023-09-04 12:34:15 +0600

    updated for deploy

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/<slug:hotel_id>/', views.hotel_details, name='hotel_details'),
+    path('hotel/<slug:hotel_id>', views.hotel_details, name='hotel_details'),
     path('search/', views.search, name='search'),
     path('review/<int:hotel_id>/', views.submit_review, name='review'),
     path('edit/<int:review_id>/', views.edit_review, name='edit_review'),
