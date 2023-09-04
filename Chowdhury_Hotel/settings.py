253943cefb8c371e9ce74fcdaa77e28e96588ce1
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2023-09-04 12:10:33 +0600

    updated for deploy

--- Chowdhury_Hotel/settings.py
+++ Chowdhury_Hotel/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#1#ltg3sa$9@&9z8%px53bfgr^3&f9#9$fftgo+e@pnz0ce^m*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
