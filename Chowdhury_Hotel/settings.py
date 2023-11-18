6154df7ec3306482085c04fd99fbc9972b24aa3f
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2023-11-18 12:16:58 +0600

    working

--- Chowdhury_Hotel/settings.py
+++ Chowdhury_Hotel/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR/'static',
-]
-print(STATICFILES_DIRS)
+STATIC_ROOT = BASE_DIR/'static'
+# STATICFILES_DIRS = [
+#     BASE_DIR/'static',
+# ]
+# print(STATICFILES_DIRS)
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 print(MEDIA_ROOT)
