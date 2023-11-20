b21cf822f8c249470227838110de54536fed4219
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2023-11-20 11:22:21 +0600

    completed

--- Chowdhury_Hotel/settings.py
+++ Chowdhury_Hotel/settings.py
@@ -123,14 +123,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/'static'
-# STATICFILES_DIRS = [
-#     BASE_DIR/'static',
-# ]
+# STATIC_ROOT = BASE_DIR/'static'
+STATICFILES_DIRS = [
+    BASE_DIR/'static',
+]
 # print(STATICFILES_DIRS)
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-print(MEDIA_ROOT)
+# print(MEDIA_ROOT)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
