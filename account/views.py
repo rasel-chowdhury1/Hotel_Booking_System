926dd148172052f3f2475b87dec31867ccd1fb3b
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2023-11-21 10:52:59 +0600

    authentication completed

--- account/views.py
+++ account/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .forms import RegistrationForm
 from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
 
 # Create your views here.
 def get_create_session(request):
@@ -13,10 +14,14 @@ def register(request):
     form = RegistrationForm()
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
+        
         if form.is_valid():
-            user = form.save(commit=False)
+            user = form.save()
             login(request, user)
+            messages.success(request, 'Registration successful. You are now logged in.')
             return redirect('home')
+        else:
+            print(form.errors)
         #  print(form.cleaned_data)
         
         
@@ -29,12 +34,19 @@ def sign_in(request):
     if request.method == 'POST':
         user_name = request.POST.get('username')
         password = request.POST.get('password')
-        user = authenticate(username = user_name, password = password)
-        print(user)
-        login(request, user)
+        user = authenticate(request, username = user_name, password = password)
+        # print(user)
+        # login(request, user)
         
-        #login hoye gese
-        return redirect('home')
+        if user is not None:
+            if not user.is_anonymous:
+                login(request, user)
+                return redirect('home')
+            else:
+                return render(request, 'account/signin.html', {'error_message': 'Invalid login credentials'} )
+        else:
+            return render(request, 'account/signin.html', {'error_message': 'Invalid login credentials'} )
+        
     return render(request, 'account/signin.html')
 
 def user_logout(request):
